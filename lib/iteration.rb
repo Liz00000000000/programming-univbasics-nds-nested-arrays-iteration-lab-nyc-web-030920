5f5e4ccf44f8e8c885bfa27b0c929a7bd4efc6c6
Elizabeth Karen <elizabeth.michael.karen@gmail.com>
2020-02-22 20:59:44 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -27,7 +27,7 @@ def find_greater_pair(array_2)
   return [array_2[0].max, array_2[1].max, array_2[2].max, array_2[3].max, array_2[4].max]
 end
 
-def total_even_pairs(src)
+def total_even_pairs(array_3)
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # if both numbers in the pair are even, then add both those numbers to the
   # total
@@ -35,15 +35,12 @@ def total_even_pairs(src)
   # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
   # the number was even. Review the operator documentation if you've forgotten
   # this!
+  array_3 = [[86262, 58115], [22558, 97709], [66236, 28056], [25992, 81375], [11248, 56789], [42783, 27353], [70796, 63432], [53234, 63058], [72316, 8675309]]
   sum = 0
-  src.each do | num1, num2 |
+  array_3.each do | num1, num2 |
     if num1 % 2 == 0 && num2 % 2==0
       sum += num1 + num2
     end
   end
   puts sum
 end
-
-array_3 = [[86262, 58115], [22558, 97709], [66236, 28056], [25992, 81375], [11248, 56789], [42783, 27353], [70796, 63432], [53234, 63058], [72316, 8675309]]
-
-total_even_pairs(array_3)
